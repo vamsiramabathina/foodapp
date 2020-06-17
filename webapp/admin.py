c0b4f06df49d81997bfc87a82375a983fa4ddc7b
vamsiramabathina <vamsiramabathina@gmail.com>
2020-06-17 11:47:30 +0530

    Initial commit

---
+++ webapp/admin.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from .models import Customer,Restaurant,Item,Menu,Order,orderItem,User
+
+admin.site.register(User)
+admin.site.register(Customer)
+admin.site.register(Restaurant)
+admin.site.register(Item)
+admin.site.register(Menu)
+admin.site.register(Order)
+admin.site.register(orderItem)
